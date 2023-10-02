effc10cb484723090708eca15c18763ff678c7fa
Jonas Peeters <jonas.peeters@icloud.com>
2023-10-02 15:47:20 +0000

    Destroy?

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -4062,6 +4062,7 @@ arc_evict_blk(spa_t *spa, const blkptr_t *bp) {
 		if (HDR_HAS_L1HDR(hdr)) {
 			uint64_t bytes_evicted;
 			arc_evict_hdr(hdr, &bytes_evicted);
+			arc_hdr_destroy(hdr);
 		} else {
 			zfs_dbgmsg("arc_evict_blk: hdr %p has no L1", hdr);
 		}
