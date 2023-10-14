521df6ff2d082cd663b59abe3967ddb13a411b41
Jonas Peeters <jonas.peeters@icloud.com>
2023-10-14 17:50:59 +0000

    lg

--- module/zfs/zio.c
+++ module/zfs/zio.c
@@ -2242,6 +2242,7 @@ static inline void
 __zio_execute(zio_t *zio)
 {
 	ASSERT3U(zio->io_queued_timestamp, >, 0);
+	zfs_dbgmsg("prio: %d", zio->io_priority);
 
 	while (zio->io_stage < ZIO_STAGE_DONE) {
 		enum zio_stage pipeline = zio->io_pipeline;
